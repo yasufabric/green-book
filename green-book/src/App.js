b0e7cbd378c7afb8fb8f2adfc5401a34c0f5ed2c
佐藤泰治 <y.sato@impl.co.jp>
2019-03-03 14:06:43 +0900

    added moment

--- green-book/src/App.js
+++ green-book/src/App.js
@@ -1,27 +1,26 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import AnalogClock, { Themes } from "react-analog-clock";
 import { Form, Input } from "reactstrap";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      minute: ""
+    };
+  }
   render() {
     return (
       <div className="App">
         <AnalogClock theme={Themes.dark} />
         <Form>
-          <Input placeholder="lg" bsSize="lg" />
-          <Input placeholder="default" />
-          <Input placeholder="sm" bsSize="sm" />
-          <Input type="select" bsSize="lg">
-            <option>Large Select</option>
-          </Input>
-          <Input type="select">
-            <option>Default Select</option>
-          </Input>
-          <Input type="select" bsSize="sm">
-            <option>Small Select</option>
-          </Input>
+          <Input
+            placeholder="lg"
+            bsSize="lg"
+            value={this.state.minute}
+            onChange={e => this.setState({ minute: e.target.value })}
+          />
         </Form>
       </div>
     );
