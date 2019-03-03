db0820bb0603ece9349d4ec939fd5a18b7449b88
佐藤泰治 <y.sato@impl.co.jp>
2019-03-03 13:55:09 +0900

    added forms

--- green-book/src/App.js
+++ green-book/src/App.js
@@ -1,13 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import AnalogClock, { Themes } from 'react-analog-clock';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import AnalogClock, { Themes } from "react-analog-clock";
+import { Form, Input } from "reactstrap";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <AnalogClock theme={Themes.dark} />
+        <Form>
+          <Input placeholder="lg" bsSize="lg" />
+          <Input placeholder="default" />
+          <Input placeholder="sm" bsSize="sm" />
+          <Input type="select" bsSize="lg">
+            <option>Large Select</option>
+          </Input>
+          <Input type="select">
+            <option>Default Select</option>
+          </Input>
+          <Input type="select" bsSize="sm">
+            <option>Small Select</option>
+          </Input>
+        </Form>
       </div>
     );
   }
